1d50db637f6b14f86992f83e12f96de017b53a8e
shane tully <shane@shanetully.com>
2013-09-07 05:24:59 +0000

    get application name for listview

--- src/com/pennapps/disposableapps/AlarmAdapter.java
+++ src/com/pennapps/disposableapps/AlarmAdapter.java
@@ -3,6 +3,10 @@ package com.pennapps.disposableapps;
 import java.util.ArrayList;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,11 +18,14 @@ import android.widget.TextView;
 public class AlarmAdapter extends ArrayAdapter<Alarm> {
     private Context context;
     private ArrayList<Alarm> alarms;
+    private PackageManager pm;
     
     public AlarmAdapter(Context context, ArrayList<Alarm> alarms) {
         super(context, R.layout.alarm_adapter, alarms);
         this.context = context;
         this.alarms = alarms;
+
+        pm = context.getPackageManager();
     }
     
     public View getView(final int position, View view, ViewGroup parent) {
@@ -30,11 +37,29 @@ public class AlarmAdapter extends ArrayAdapter<Alarm> {
         
         TextView alarmNameLabel = (TextView) view.findViewById(R.id.alarmNameLabel);
         TextView timeLeftLabel = (TextView) view.findViewById(R.id.timeLeftLabel);
+
+        ApplicationInfo ai = getApplicationInfo(alarms.get(position).getPackageUri());
         
         // Set the name on the label
-        alarmNameLabel.setText(alarms.get(position).getPackageUri() + ":");
-        timeLeftLabel.setText(alarms.get(position).getPackageUri() + ":");
+        alarmNameLabel.setText(getApplicationLabel(ai));
+        timeLeftLabel.setText(getApplicationLabel(ai));
         
         return view;
+    }
+
+    private ApplicationInfo getApplicationInfo(final Uri packageUri) {
+        try {
+            return pm.getApplicationInfo(packageUri.toString().replace("package:", ""), 0);
+        } catch (final PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    private String getApplicationLabel(final ApplicationInfo ai) {
+        return pm.getApplicationLabel(ai).toString();
+    }
+
+    private Drawable getApplicationIcon(final ApplicationInfo ai) {
+        return pm.getApplicationIcon(ai);
     }
 }
