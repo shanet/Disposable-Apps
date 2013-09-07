4d3399dcc9cf2604350d5ebda6c189d9c6111022
Joseph Donofry <Rubberduckie3554@gmail.com>
2013-09-07 04:48:39 -0400

    Fixed BOOT_COMPLETED not being received

--- src/com/pennapps/disposableapps/PackageReceiver.java
+++ src/com/pennapps/disposableapps/PackageReceiver.java
@@ -18,28 +18,10 @@ public class PackageReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Uri packageUri = intent.getData();
 
-        // Start the app install activity
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            Database alarmDb = new Database(context);
-            ArrayList<Alarm> alarms = alarmDb.selectAllAlarms();
-
-            for (Alarm info : alarms) {
-                long alarmDate = info.getAlarmDate().getTime();
-                long currentDate = System.currentTimeMillis();
-
-                long alarmSetDate = alarmDate - currentDate;
-
-                if (alarmSetDate > 0) {
-                     Utils.setUninstallTimer(context, alarmSetDate, info.getPackageUri());
-                } else {
-                    Utils.setUninstallTimer(context, 10000, info.getPackageUri());
-                }
-            }
-        } else if (intent.getAction().equals(Intent.ACTION_INSTALL_PACKAGE) || intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
-            intent = new Intent(context, AppInstalledActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.putExtra("packageUri", packageUri);
-            context.startActivity(intent);
-        }
+        intent = new Intent(context, AppInstalledActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("packageUri", packageUri);
+        Log.d("DisposableApps", packageUri.toString());
+        context.startActivity(intent);
     }
 }
